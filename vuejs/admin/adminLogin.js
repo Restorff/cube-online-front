108d638beed57ef38fd6e3bf88d8d95d2c9cff91
liaotao421 <1984457659@qq.com>
2020-10-15 12:35:49 +0800

    登录滑块验证

--- vuejs/admin/adminLogin.js
+++ vuejs/admin/adminLogin.js
@@ -3,34 +3,14 @@ var form_login = new Vue({
 
     methods: {
         login: function() {
-            // alert(sessionStorage.getItem('username'));
-            var _this = this;
             var formser = $('#form_login').serialize();
-            // console.log(formser)
-            axios.post("http://cube-online.lstf666.cn:8083/Cube-Online/admin/login", formser).then(
-                function(response) {
-                    console.log(response);
-                    if (response.data.type == "error") {
-                        alert(response.data.msg + ',请重新输入')
-                    } else {
-                        sessionStorage['adminUser'] = response.data.admin.username;
-                        if (response.data.isRoot == '1') {
-                            alert('您是本站超级管理员,' + response.data.msg + ",点击确定，跳转到主页面");
-                            window.location.href = "/rootLogined.html";
-                        } else {
-                            alert('您是本站普通管理员,' + response.data.msg + ",点击确定，跳转到主页面");
-                            window.location.href = "/gameList.html";
-                        }
-                        // 将信息存入session
-
-                        // alert(sessionStorage.getItem('username'));
-                    }
-                }
-            ).catch(
-                function(error) {
-                    console.log("发生了错误：" + error);
-                })
-
+            console.log(formser)
+            javaex.dialog({
+                type : "dialog",	// 弹出层类型
+                width : "330",
+                height : "210",
+                url : "check.html?"+formser+"&isAdmin=1"
+            });
         }
     }
 })
